c8b233b26ef82d623b4b8c0e1e23e3fc014f32f7
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2022-08-14 00:37:19 +0530

    added mongoose connect

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import customersRouter from './routes/customersRouter.js'
 
 const app = new express()
+
+mongoose
+    .connect('mongodb://localhost:27017/services-db')
+    .then(() => {
+        console.log('connected to mongodb')
+    })
+    .catch(err => {
+        console.log('error connecting to mongodb : ', err)
+    })
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello Express!' })
 })
+
+app.use('/customers', customersRouter)
 
 app.listen(3001, () => {
     console.log('Server is running at http://localhost:3001')
