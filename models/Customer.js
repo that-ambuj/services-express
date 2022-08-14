23aba11c5729b99f4166b8127d8d77ff2f340a55
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2022-08-14 15:24:39 +0530

    remove some data for security purposes

--- models/Customer.js
+++ models/Customer.js
@@ -28,4 +28,14 @@ const customerSchema = new mongoose.Schema({
     sessions: [{ type: String }],
 })
 
+customerSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+        delete returnedObject.sessions
+    },
+})
+
 export default mongoose.model('Customer', customerSchema)
